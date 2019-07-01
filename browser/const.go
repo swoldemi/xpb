5651be41f88d46c348b9590afd4918e77105119d
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2019-07-01 15:29:02 +0900

    Begin adding selectors and constants to const.go

--- browser/const.go
+++ browser/const.go
@@ -1,5 +1,12 @@
 package browser
 
 const (
-	Something = "something"
+	// OwnerRole defines the name of the Owner role
+	OwnerRole = "Owner"
+
+	// OwnerRoleSelector defines the CSS selector used to find the OwnerRole element
+	OwnerRoleSelector = "#mat-option-124"
+
+	// BillingRole defines the name of the Project Billing Manager role.
+	BillingRole = "Project Billing Manager"
 )
