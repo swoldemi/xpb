18122c5005ff7fb3f5816778157160f48da70a44
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2019-07-01 15:28:23 +0900

    Complete initial majority of InviteGuest functionality

--- browser/chrome.go
+++ browser/chrome.go
@@ -77,11 +77,44 @@ func (g *GCPBrowser) LoginHost() error {
 	return nil
 }
 
-// InviteGuest invites the guest account, with credits
+// InviteGuest invites the guest account, with credits,
 // to the host's selected project
 func (g *GCPBrowser) InviteGuest() error {
-	gcpConsoleURL := fmt.Sprintf("https://console.cloud.google.com/home/dashboard?project=%v", g.Config.HostProjectID)
+	gcpConsoleURL := fmt.Sprintf("https://console.cloud.google.com/iam-admin/iam?authuser=0&project=%v", g.Config.HostProjectID)
 	if err := g.WebDriver.Get(gcpConsoleURL); err != nil {
+		return err
+	}
+	time.Sleep(time.Second * 8) // Wait for the page the load
+
+	if err := g.clickAdd(); err != nil {
+		return err
+	}
+	if err := g.typeGuestEmail(); err != nil {
+		return err
+	}
+
+	// A material option object gets displayed after the email is typed
+	// If this element is visible before beginning role selection,
+	// then it will prevent the WebDriver from selecting the role box by
+	// intercepting the click. To solve this, remove focus from the email
+	// field by clicking on an arbitrary element. In this case, a header
+	if err := g.clickHeader(); err != nil {
+		return err
+	}
+
+	// The owner must have the Owner and Project Billing Manager
+	// roles in order to be able to change billing accounts
+	// The drawer only displays one role entry field, by default
+	if err := g.addFirstRole(); err != nil {
+		return err
+	}
+	if err := g.clickAddAnother(); err != nil {
+		return err
+	}
+	if err := g.addSecondRole(); err != nil {
+		return err
+	}
+	if err := g.submitGuestInvite(); err != nil {
 		return err
 	}
 	time.Sleep(time.Second * 10)
--- browser/console.go
+++ browser/console.go
@@ -1,1 +1,175 @@
 package browser
+
+import (
+	"time"
+
+	"github.com/tebeka/selenium"
+)
+
+func (g *GCPBrowser) clickAdd() error {
+	addBtn, err := g.WebDriver.FindElement(selenium.ByXPATH, "//button[contains(@aria-label,\"Add member\")]")
+	if err != nil {
+		return err
+	}
+
+	err = addBtn.Click()
+	if err != nil {
+		return err
+	}
+
+	time.Sleep(time.Second * 8) // Need to wait for the drawer to open
+	return nil
+}
+
+func (g *GCPBrowser) typeGuestEmail() error {
+	emailField, err := g.WebDriver.FindElement(selenium.ByCSSSelector, "#mat-input-1")
+	if err != nil {
+		return err
+	}
+
+	err = emailField.SendKeys(g.Config.NamedGuestEmail)
+	if err != nil {
+		return err
+	}
+
+	err = emailField.SendKeys(selenium.EnterKey)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (g *GCPBrowser) clickHeader() error {
+	headerEl, err := g.WebDriver.FindElement(selenium.ByCSSSelector, "#cfc-subtask-heading-0")
+	if err != nil {
+		return err
+	}
+
+	err = headerEl.Click()
+	if err != nil {
+		return err
+	}
+
+	time.Sleep(time.Second * 1) // Wait for the option to be gone
+	return nil
+}
+
+func (g *GCPBrowser) addFirstRole() error {
+	roleField, err := g.WebDriver.FindElement(selenium.ByCSSSelector, "#mat-form-field-label-3")
+	if err != nil {
+		return err
+	}
+	err = roleField.Click()
+	if err != nil {
+		return err
+	}
+	time.Sleep(time.Second * 1) // Wait for the menu to be visible
+
+	menuInput, err := g.WebDriver.FindElement(selenium.ByXPATH, "//input[@placeholder=\"undefined\"]")
+	if err != nil {
+		return err
+	}
+
+	err = menuInput.SendKeys(OwnerRole)
+	if err != nil {
+		return err
+	}
+
+	// The filter has completed, click the role
+	ownerRoleEl, err := g.WebDriver.FindElement(selenium.ByCSSSelector, OwnerRoleSelector)
+	if err != nil {
+		return err
+	}
+	err = ownerRoleEl.Click()
+	if err != nil {
+		return err
+	}
+
+	time.Sleep(time.Second * 1) // Wait for the menu to close
+	return nil
+}
+
+func (g *GCPBrowser) clickAddAnother() error {
+	addRoleBtn, err := g.WebDriver.FindElement(selenium.ByXPATH, "//*[@icon=\"add\"]")
+	if err != nil {
+		return err
+	}
+
+	err = addRoleBtn.Click()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (g *GCPBrowser) addSecondRole() error {
+	roleFields, err := g.WebDriver.FindElements(selenium.ByXPATH, "//cfc-iam-role-picker[@placeholder=\"Select a role\"]")
+	if err != nil {
+		return err
+	}
+
+	// Click the second element that matches the selector
+	err = roleFields[1].Click()
+	if err != nil {
+		return err
+	}
+	time.Sleep(time.Second * 1) // Wait for the menu to be visible
+
+	menuInput, err := g.WebDriver.FindElement(selenium.ByXPATH, "//input[@placeholder=\"undefined\"]")
+	if err != nil {
+		return err
+	}
+
+	// Because this is the second time we're opening the menu, clear it
+	err = menuInput.Clear()
+	if err != nil {
+		return err
+	}
+
+	err = menuInput.SendKeys(BillingRole)
+	if err != nil {
+		return err
+	}
+
+	// The menu has been filtered to show ONLY the Project Billing Manager Role,
+	// but because the Project Billing Manager is assigned a, seemingly,
+	// random option ID on refresh, we can't give it a static selector.
+	// Instead, do a search on the document tree. Stop when the
+	// inner text of the element matches the name of the role
+	optionElements, err := g.WebDriver.FindElements(selenium.ByXPATH, "//mat-option[starts-with(@id, \"mat-option\")]")
+	if err != nil {
+		return err
+	}
+	_ = optionElements
+
+	// Find the only one that is visible, the Project Billing Manager button and click it
+	for _, element := range optionElements {
+		displayed, err := element.IsDisplayed()
+		if err != nil {
+			return err
+		}
+
+		if displayed {
+			err = element.Click()
+			if err != nil {
+				return err
+			}
+			break
+		}
+	}
+
+	return nil
+}
+
+func (g *GCPBrowser) submitGuestInvite() error {
+	submitBtn, err := g.WebDriver.FindElement(selenium.ByXPATH, "//*[@cfcformsubmit=\"addRoleForm\"]")
+	if err != nil {
+		return err
+	}
+
+	err = submitBtn.Click()
+	if err != nil {
+		return err
+	}
+	return nil
+}
